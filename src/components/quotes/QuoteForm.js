a286063680265fac1cba254dcfb8dadbdf90e812
Veronika <verona.binko@gmail.com>
2021-12-15 10:52:13 +0300

    Feat: add component Prompt in QuoteForm.js for warning user about lost his data after leaving page

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState, Fragment } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,8 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+
+  const [isEntering, setIsEntering] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -18,10 +21,23 @@ const QuoteForm = (props) => {
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
+  
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
+  const formFocusedHandler = () => {
+    setIsEntering(true);
+  };
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+    <Fragment>
+      <Prompt 
+        when={isEntering}
+        message={(location) => 'Are you sure want to live the page? Your data will be lost!'}
+      />
+      <Card>
+      <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +53,12 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishEnteringHandler} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
+    
   );
 };
 
