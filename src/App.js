91b8b0dfb4fb54561ccfbc5308cc00eaff3aea88
Veronika <verona.binko@gmail.com>
2021-12-21 15:31:02 +0300

    Refactor: in App.js change imports on const and add Suspense

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import AllQuotes from './pages/AllQuotes';
-import NewQuotes from './pages/NewQuote';
-import QuotesDetail from './pages/QuotesDetail';
+
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuotes = React.lazy(() => import('./pages/NewQuote'));
+const QuotesDetail = React.lazy(() => import('./pages/QuotesDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes'/>
-      </Route>
-      <Route path='/quotes' exact>
-        <AllQuotes />
-      </Route>
-      <Route path='/quotes/:quotesId'>
-        <QuotesDetail />
-      </Route>
-      <Route path='/new-quote'>
-       <NewQuotes />
-      </Route>
-      <Route path='*'>
-        <NotFound />
-      </Route>
-    </Switch>
+      <Suspense 
+        fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes'/>
+        </Route>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quotesId'>
+          <QuotesDetail />
+        </Route>
+        <Route path='/new-quote'>
+         <NewQuotes />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
